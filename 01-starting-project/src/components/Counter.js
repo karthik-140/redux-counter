dfa59e7a4848b55c0b9daa951fca7ca6df2f321a
karthik-140 <shanigaramkarthik@gmail.com>
2023-03-03 12:08:52 +0530

    counter-manipulation

---
+++ 01-starting-project/src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux';
+import classes from './Counter.module.css';
+
+const Counter = () => {
+  
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch();
+
+  const incrementHandler = () =>{
+    dispatch({type: 'incrementBy2'});
+  }
+  const decrementHandler = () =>{
+    dispatch({type: 'decrementBy2'});
+  }
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
