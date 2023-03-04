cb464e1a0974a999d8433ea3f96edb9a2c9f7386
karthik-140 <shanigaramkarthik@gmail.com>
2023-03-04 10:16:53 +0530

    implementing-login-logout-using-redux-toolkit

--- 01-starting-project/src/components/Counter.js
+++ 01-starting-project/src/components/Counter.js
@@ -1,27 +1,34 @@
 import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
+import { counterActions } from '../store/index';
 
 const Counter = () => {
   
-  const counter = useSelector(state => state.counter)
+  const counter = useSelector(state => state.counter.counter)
+  const show = useSelector(state => state.counter.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () =>{
-    dispatch({type: 'incrementBy5'});
+    dispatch(counterActions.increment());
+  }
+  const increaseHandler = () =>{
+    dispatch(counterActions.increase(10));
   }
   const decrementHandler = () =>{
-    dispatch({type: 'decrementBy5'});
+    dispatch(counterActions.decrement());
   }
-
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={incrementHandler}>Increment By 5</button>
-        <button onClick={decrementHandler}>Decrement By 5</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment By 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
